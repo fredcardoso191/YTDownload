6a831f31ef61cc3a3c8684aa725a5158cda99a80
Fred Cardoso <fredcardoso01@hotmail.com>
2022-07-09 17:32:18 -0300

    add new options

--- main.py
+++ main.py
@@ -6,14 +6,25 @@ import validation as va
 while True:
     url = str(input(los.enterurl()))
 
-    if url == "quit":
+    if url == 'quit':
         exit()
     if (va.isValidURL(url) == True):
-        op = str(input(los.option())) or '.'
-        if op == '1':
-            download.video(url)
-        if op == '2':
-            download.audio(url)
+        while True:
+            op = str(input(los.option())) or '.'
+            if op == '1':
+                download.video(url)
+                break
+            if op == '2':
+                download.audio(url)
+                break
+            if op == 'quit':
+                exit()
+            if op == 'back':
+                break
+            else:
+                print(los.invalidoption())
+                os.system("pause")
+                os.system("cls")
     else:
         print(los.invalidurl(url))
     
