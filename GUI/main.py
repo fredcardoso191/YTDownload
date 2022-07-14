46a78500f1388af36a3a32287b272f2a57bd8ce6
Fred Cardoso <fredcardoso01@hotmail.com>
2022-07-13 22:00:48 -0300

    adjusting button position

--- GUI/main.py
+++ GUI/main.py
@@ -1,27 +1,14 @@
-import tkinter as tk
 from tkinter import *
 from tkinter import messagebox
 from pytube import YouTube
 from validation import isValidURL
 import language_os as los
+import layout
 import getpass
 import pathlib
 
 drive = pathlib.Path.home().drive
 user = getpass.getuser()
-
-class HoverButton(tk.Button):
-    def __init__(self, master, **kw):
-        tk.Button.__init__(self,master=master,**kw)
-        self.defaultBackground = self["background"]
-        self.bind("<Enter>", self.on_enter)
-        self.bind("<Leave>", self.on_leave)
-
-    def on_enter(self, e):
-        self['background'] = self['activebackground']
-
-    def on_leave(self, e):
-        self['background'] = self.defaultBackground
 
 def main():
     def video(enUrl):
@@ -65,13 +52,12 @@ def main():
         enUrl.unbind('<Button-1>', on_click_id)
 
     root = Tk()
-
     root.resizable(width=False, height=False)
     root.title("Youtube Download")
     root.iconbitmap('icon.ico')
 
-    width = 700
-    height = 50
+    width = 600
+    height = 57
 
     width_screen = root.winfo_screenwidth()
     height_screen = root.winfo_screenheight()
@@ -81,15 +67,15 @@ def main():
 
     root.geometry("%dx%d+%d+%d" % (width, height, posx, posy))
 
-    enUrl = Entry(root, width=400, font="Arial")
-    enUrl.pack(ipady=1)
+    enUrl = Entry(root, width=70, font="Arial")
+    enUrl.place(x=0, y=0)
     enUrl.insert(0, "https://www.youtube.com/watch?v=")
     enUrl.configure(state=DISABLED)
 
     on_click_id = enUrl.bind('<Button-1>', on_click)
 
-    btnVideo = HoverButton(root, text=los.button("video"), activebackground='#fcf5ff', command=lambda: video(enUrl)).place(x=0, y=24)
-    btnAudio = HoverButton(root, text=los.button("audio"), activebackground='#fcf5ff',command=lambda: audio(enUrl)).place(x=95, y=24)
+    btnVideo = layout.HoverButton(root, text=los.button("video"), width=13, activebackground='#fcf5ff', command=lambda: video(enUrl)).place(x=4, y=28)
+    btnAudio = layout.HoverButton(root, text=los.button("audio"), width=13, activebackground='#fcf5ff',command=lambda: audio(enUrl)).place(x=110, y=28)
 
     root.mainloop()
 main()
